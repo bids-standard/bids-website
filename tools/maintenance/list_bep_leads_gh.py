26f33a3415bb9106c2bde98bae2db48606c108e5
Remi Gau <remi_gau@hotmail.com>
2024-08-01 18:54:13 +0200

    split by given and family names

--- tools/maintenance/list_bep_leads_gh.py
+++ tools/maintenance/list_bep_leads_gh.py
@@ -1,13 +1,27 @@
 """List the emails of the leader of each BEP."""
 
+from pathlib import Path
+
 import pandas as pd
-from bids_website.utils import data_dir
+from bids_website.utils import bids_spec_dir, data_dir
+from rich import print
 from ruamel.yaml import YAML
+
+yaml = YAML(typ="safe", pure=True)
+
+
+def load_citation(citation_file: Path) -> dict:
+    """Load `CITATION.cff` file."""
+    with open(citation_file, "r", encoding="utf8") as input_file:
+        return yaml.load(input_file)
+
+
+citation = load_citation(bids_spec_dir() / "CITATION.cff")
+print(citation)
 
 beps = data_dir() / "beps" / "beps.yml"
 
 with open(beps, "r") as f:
-    yaml = YAML(typ="safe", pure=True)
     data = yaml.load(f)
 
 output = {"bep": [], "lead": [], "email": []}
@@ -15,7 +29,7 @@ output = {"bep": [], "lead": [], "email": []}
 for bep in data:
     for lead in bep["leads"]:
         output["bep"].append(f'{bep["number"]} - {bep["title"]}')
-        output["lead"].append(f'{lead["given-names"]} {lead["family-names"]}')
+        output["lead"].append(f'{lead["name"]}')
         output["email"].append(f'{lead["email"]}')
 
 df = pd.DataFrame(output)
